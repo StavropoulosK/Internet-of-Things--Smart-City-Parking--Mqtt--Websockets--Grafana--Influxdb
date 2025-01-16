88a3d3358603dae967253094ba4fab59d5fc379d
Hlias <iliouzoun@outlook.com>
2025-01-16 16:52:35 +0200

    made frontpage prettier

--- public/js/admin/loggedIn.js
+++ public/js/admin/loggedIn.js
@@ -2,12 +2,20 @@ async function checkLoginStatus() {
     try {
         const response = await fetch('/admin/is-logged-in');
         const data = await response.json();
+        
+        console.log('response:', data.loggedIn);
 
         // Change button text if logged in
         if (data.loggedIn) {
-            const loginButton = document.getElementById('loginButton');
+            const loginButton = document.getElementById('login-button');
             loginButton.href = '/admin/logout';
             loginButton.innerText = 'Logout';
+
+            const dashboards = document.getElementById('dashboards');
+            dashboards.style.display = 'block';
+        } else {
+            const dashboards = document.getElementById('dashboards');
+            dashboards.style.display = 'none';
         }
     } catch (error) {
         console.error('Error checking login status:', error);
